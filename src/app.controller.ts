2d586ccb134e81e21112ae7616c997f6a24c3eab
ryongseong <ryongseong.dev@gmail.com>
2025-12-07 19:13:47 +0900

    feat: Apply configuration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ip } from './decorators/ip.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(@Ip() ip: string): string {
+    console.log(this.configService.get<string>('ENVIRONMENT'));
     return this.appService.getHello();
   }
 
