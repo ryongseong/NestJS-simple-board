18add0f1c25fa98a915dece68f4acd5ec97d1b16
ryongseong <ryongseong.dev@gmail.com>
2025-12-07 19:01:13 +0900

    feat: Apply logger module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,14 @@
-import {
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ip } from './decorators/ip.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+  getHello(@Ip() ip: string): string {
+    return this.appService.getHello();
   }
 
   @Get('name')
