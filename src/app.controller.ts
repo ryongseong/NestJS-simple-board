9e2efaa5211fb2471d8c5dc47514ce63cf3ded16
ryongseong <ryongseong.dev@gmail.com>
2025-12-07 18:48:49 +0900

    feat: Apply custom exception globally

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import { Ip } from './decorators/ip.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Ip() ip: string): string {
-    console.log('Client IP:', ip);
-    return this.appService.getHello();
+  getHello(): string {
+    throw new HttpException('Not found', HttpStatus.NOT_FOUND);
   }
 
   @Get('name')
